8ecdecde0b08c0078a1766fce17055ddc3c60c6b
Nick Taylor <nick@iamdeveloper.com>
2020-03-08 00:33:55 -0500

    Fixed viewport meta tag.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,12 +13,12 @@ export default class Body extends React.Component {
                 <Helmet>
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
                     <meta charSet="utf-8"/>
-                    <meta name="viewport" content="width=device-width, initialScale=1.0" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <meta name="google" content="notranslate" />
                     <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
-                    {(_.get(this.props, 'pageContext.frontmatter.template') === 'post') &&  
-                    _.get(this.props, 'pageContext.frontmatter.canonical_url') && 
+                    {(_.get(this.props, 'pageContext.frontmatter.template') === 'post') &&
+                    _.get(this.props, 'pageContext.frontmatter.canonical_url') &&
                     <link rel="canonical" href={_.get(this.props, 'pageContext.frontmatter.canonical_url')}/>
                     }
                 </Helmet>
