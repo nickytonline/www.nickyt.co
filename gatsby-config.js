700781c6b3c860918e3d393384f096523ddc33ed
Nick Taylor <nick@iamdeveloper.com>
2020-03-08 01:06:59 -0500

    Added service worker.

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,21 @@ module.exports = {
                 pageContextProperty: `menus`,
                 menus: require('./src/data/menus.json'),
             }
-        }
+        },
+        'gatsby-plugin-offline',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'Just some dev',
+                short_name: 'Just some dev',
+                start_url: '/',
+                background_color: '#f7f0eb',
+                theme_color: '#a2466c',
+                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+                display: 'standalone',
+                icon: 'static/images/apple-touch-icon.png',
+            },
+        },
     ]
 };
