ae7c80929b1286bf197f3e89ca2db9aab89d490b
Nick Taylor <nick@iamdeveloper.com>
2020-03-19 08:21:44 -0400

    Put back GA on new site.

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,5 +54,19 @@ module.exports = {
         },
         'gatsby-plugin-postcss',
         'gatsby-plugin-sitemap',
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: 'UA-55732414-1',
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ['/preview/**', '/do-not-track/me/too/'],
+            },
+        },
     ],
 };
