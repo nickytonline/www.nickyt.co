622f4172fd0d8401a02188aa7915b664e336aa79
Nick Taylor <nick@iamdeveloper.com>
2020-09-09 16:31:19 -0400

    Fixed social links semantic HTML violations.

--- src/components/Social.js
+++ src/components/Social.js
@@ -11,20 +11,22 @@ export default class Social extends React.Component {
                     _.get(this.props, 'pageContext.site.data.social.links'),
                     (link, link_idx) =>
                         link && (
-                            <Link
-                                key={link_idx}
-                                to={_.get(link, 'url')}
-                                target="_blank"
-                                rel="noopener"
-                            >
-                                <span
-                                    className={'fab ' + _.get(link, 'icon')}
-                                    aria-hidden="true"
-                                />
-                                <span className="screen-reader-text">
-                                    {_.get(link, 'title')}
-                                </span>
-                            </Link>
+                            <li>
+                                <Link
+                                    key={link_idx}
+                                    to={_.get(link, 'url')}
+                                    target="_blank"
+                                    rel="noopener"
+                                >
+                                    <span
+                                        className={'fab ' + _.get(link, 'icon')}
+                                        aria-hidden="true"
+                                    />
+                                    <span className="screen-reader-text">
+                                        {_.get(link, 'title')}
+                                    </span>
+                                </Link>
+                            </li>
                         ),
                 )}
             </ul>
