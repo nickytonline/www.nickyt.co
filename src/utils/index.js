84213af26c20b504d0a42daa9c84b0bf4f6a189c
Nick Taylor <nick@iamdeveloper.com>
2020-12-05 17:28:23 -0500

    Removed Gatsby.

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-export { default as cycler } from './cycler';
-export { default as getPage } from './getPage';
-export { default as getPages } from './getPages';
-export { default as htmlToReact } from './htmlToReact';
-export { default as toUrl } from './toUrl';
-export { default as markdownify } from './markdownify';
-export { default as safePrefix } from './safePrefix';
-export { default as toStyleObj } from './toStyleObj';
-export { default as Link } from './link';
