ec67721ac05861a7af7698619da30f1254f4d7b0
Nick Taylor <nick@iamdeveloper.com>
2020-12-05 17:56:52 -0500

    Fixed stackbit script for Eleventy deploy.

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -8,8 +8,8 @@ curl -s -X POST https://api.stackbit.com/project/5e6478cc14b6920012730a0a/webhoo
 if [[ -z "${STACKBIT_API_KEY}" ]]; then
     echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
 else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5e6478cc14b6920012730a0a 
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5e6478cc14b6920012730a0a
 fi
 curl -s -X POST https://api.stackbit.com/project/5e6478cc14b6920012730a0a/webhook/build/ssgbuild > /dev/null
-gatsby build
+npm run production
 curl -s -X POST https://api.stackbit.com/project/5e6478cc14b6920012730a0a/webhook/build/publish > /dev/null
