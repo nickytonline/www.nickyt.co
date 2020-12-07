b3d8f2e7d5bf72adf74fdf331966ab1a16ac53ab
Nick Taylor <nick@iamdeveloper.com>
2020-12-06 21:27:05 -0500

    Removed thanks and contact pages from excludes in service worker as they are static for my site.

--- src/_includes/partials/global/service-worker.js
+++ src/_includes/partials/global/service-worker.js
@@ -8,16 +8,14 @@ const EXCLUDED_URLS = [
   'admin',
   '.netlify',
   'https://identity.netlify.com/v1/netlify-identity-widget.js',
-  'https://unpkg.com/netlify-cms@^2.9.3/dist/netlify-cms.js',
-  '/contact',
-  '/thank-you'
+  'https://unpkg.com/netlify-cms@^2.9.3/dist/netlify-cms.js'
 ];
 
 // URLS that we want to be cached when the worker is installed
 const PRE_CACHE_URLS = ['/', '/fonts/lora-v13-latin-700.woff'];
 
 // You might want to bypass a certain host
-const IGNORED_HOSTS = ['localhost', 'unpkg.com', ];
+const IGNORED_HOSTS = ['localhost', 'unpkg.com'];
 
 /**
  * Takes an array of strings and puts them in a named cache store
