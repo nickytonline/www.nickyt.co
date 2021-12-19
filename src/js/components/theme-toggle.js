363d77800cc292b4623d2de5d2d807411feac27e
Nick Taylor <nick@iamdeveloper.com>
2021-12-18 23:37:46 -0500

    Removed inline style from testing something out.

--- src/js/components/theme-toggle.js
+++ src/js/components/theme-toggle.js
@@ -73,10 +73,7 @@ class ThemeToggle extends HTMLElement {
     this.innerHTML = html`
       <div class="theme-toggle">
         <div role="status" class="[ visually-hidden ][ js-mode-status ]"></div>
-        <button
-          class="button js-mode-toggle"
-          style="background-color: transparent;color: var(--color-text)"
-        >
+        <button class="button js-mode-toggle">
           ${darkModeIcon}
         </button>
       </div>
