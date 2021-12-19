2198050999adaacfb8045b779ec2284d5dfeb8b3
Nick Taylor <nick@iamdeveloper.com>
2021-12-18 23:08:17 -0500

    Now theme toggler uses icons with aria.

--- src/js/components/theme-toggle.js
+++ src/js/components/theme-toggle.js
@@ -1,5 +1,7 @@
 // For syntax highlighting only
 const html = String.raw;
+const darkModeIcon = `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
+const lightModeIcon = `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`;
 
 class ThemeToggle extends HTMLElement {
   constructor() {
@@ -58,18 +60,24 @@ class ThemeToggle extends HTMLElement {
   }
 
   setButtonLabelAndStatus(currentSetting) {
-    this.modeToggleButton.innerText = `${
-      currentSetting === 'dark' ? 'Light' : 'Dark'
-    } theme`;
+    this.modeToggleButton.setAttribute(
+      'aria-label',
+      `${currentSetting === 'dark' ? 'Light' : 'Dark'} theme`
+    );
+    this.modeToggleButton.innerHTML =
+      currentSetting === 'dark' ? lightModeIcon : darkModeIcon;
     this.modeStatusElement.innerText = `Color mode is now "${currentSetting}"`;
   }
 
   render() {
     this.innerHTML = html`
-      <div class="[ theme-toggle ] [ md:ta-right gap-top-500 ]">
+      <div class="theme-toggle">
         <div role="status" class="[ visually-hidden ][ js-mode-status ]"></div>
-        <button class="[ button ] [ font-base text-base weight-bold ] [ js-mode-toggle ]">
-          Dark theme
+        <button
+          class="button js-mode-toggle"
+          style="background-color: transparent;color: var(--color-text)"
+        >
+          ${darkModeIcon}
         </button>
       </div>
     `;
