6a4028787cd3ece9690b62434fb97fcbe875342a
Nick Taylor <nick@iamdeveloper.com>
2022-11-05 20:08:13 -0400

    chore: added automated automerge to scripts

--- bin/pr-blog-posts.sh
+++ bin/pr-blog-posts.sh
@@ -4,8 +4,8 @@ BRANCH_NAME="chore_automated_update_blog_posts_$(date +%s)"
 git branch $BRANCH_NAME
 git switch $BRANCH_NAME
 
-# This is the only file we want to commit
-git add src/pages/streaming.md
+# There are potentially multiple files if the blog post has images.
+git add .
 
 # See if we have any changes. We should.
 if [[ -n "$(git status --porcelain)" ]]; then
@@ -13,6 +13,7 @@ if [[ -n "$(git status --porcelain)" ]]; then
   git commit -m "$PR_TITLE"
   git push origin $BRANCH_NAME
   gh pr create --title "$PR_TITLE" --body "This is an automated PR to update blog posts"
+  gh pr merge --auto "$BRANCH_NAME"
 else
   # Shouldn't end up here, but log that there was nothing to sync
   echo "Looks like there was nothing to update."
