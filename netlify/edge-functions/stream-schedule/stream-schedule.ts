695c21df1389e4eeab9dfa14f273f54fd5b5f9b9
Nick Taylor <nick@iamdeveloper.com>
2023-02-26 13:53:19 -0500

    chore: added other social links for guests

--- netlify/edge-functions/stream-schedule/stream-schedule.ts
+++ netlify/edge-functions/stream-schedule/stream-schedule.ts
@@ -44,6 +44,47 @@ function buildTwitterLink({name, twitter}: {name: string; twitter: string | unde
   `;
 }
 
+function buildTwitchLink({name, twitch}: {name: string; twitch: string | undefined}) {
+  if (!twitch) {
+    return '';
+  }
+
+  const sanitizedTwitchUrl = twitch
+    .replace(/https:\/\/(www\.)?twitch\.tv\//, '')
+    .replace('@', '');
+
+  return `
+  <li><a href="https://twitch.tv/${sanitizedTwitchUrl}" title="${name}'s Twitch Profile"><span class="visually-hidden">${name}'s Twitch Profile</span> <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M21 2H3v16h5v4l4-4h5l4-4V2zm-10 9V7m5 4V7"></path></svg></a></li>
+  `;
+}
+
+function buildYoutubeLink({name, youtube}: {name: string; youtube: string | undefined}) {
+  if (!youtube) {
+    return '';
+  }
+
+  const sanitizedYoutubeUrl = youtube.replace(/https:\/\/(www\.)?youtube\.com\//, '');
+
+  return `
+  <li><a href="https://youtube.com/${sanitizedYoutubeUrl}" title="${name}'s YouTube channel"><span class="visually-hidden">${name}'s YouTube channel</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-youtube"><path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path><polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon></svg>
+  </a>
+  </li>
+  `;
+}
+
+function buildGithubLink({name, github}: {name: string; github: string | undefined}) {
+  if (!github) {
+    return '';
+  }
+
+  const sanitizedGithubUrl = github.replace(/https:\/\/(www\.)?github\.com\//, '');
+
+  return `
+    <li><a href="https://github.com/${sanitizedGithubUrl}" title="${name}'s GitHub profile"><span class="visually-hidden">${name}'s GitHub profile</span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+    </li>`;
+}
+
 function getScheduleMarkup({
   schedule,
   locale,
@@ -54,14 +95,15 @@ function getScheduleMarkup({
   timezone: string;
 }) {
   const scheduleMarkup = schedule
-    .map(({date, streamTitle, name, title, twitter, website}) => {
-      const guestDate = new Date(date).toLocaleString(locale, {
-        timeZone: timezone,
-        dateStyle: 'full',
-        timeStyle: 'long',
-      });
+    .map(
+      ({date, streamTitle, name, title, twitter, website, twitch, youtube, github}) => {
+        const guestDate = new Date(date).toLocaleString(locale, {
+          timeZone: timezone,
+          dateStyle: 'full',
+          timeStyle: 'long',
+        });
 
-      return `
+        return `
     <li class="post-list__item">
     <h2>${streamTitle}</h2>
     <time datetime="${date}">${guestDate}</time>
@@ -71,13 +113,17 @@ function getScheduleMarkup({
         <nav class="nav" aria-label="Links for live stream guest ${name}">
           <ul>
           ${buildWebsiteLink({name, website})}
+          ${buildGithubLink({name, github})}
           ${buildTwitterLink({name, twitter})}
+          ${buildTwitchLink({name, twitch})}
+          ${buildYoutubeLink({name, youtube})}
           </ul>
         </nav>
       </div>
     </li>
   `;
-    })
+      }
+    )
     .join('');
 
   return `<ol class="[ post-list__items ] [ sf-flow ] [ pad-top-300 ]">${scheduleMarkup}</ol>`;
