0e49a333deff5af31778253a82018880636ba813
Nick Taylor <nick@iamdeveloper.com>
2023-02-26 20:16:47 -0500

    chore: type cleanup

--- netlify/edge-functions/stream-schedule/StreamGuestInfo.ts
+++ netlify/edge-functions/stream-schedule/StreamGuestInfo.ts
@@ -2,11 +2,11 @@ export interface StreamGuestInfo {
   date: string;
   streamTitle: string;
   name: string;
-  title: string | undefined;
-  twitter: string | undefined;
-  youtube: string | undefined;
-  twitch: string | undefined;
-  github: string | undefined;
-  website: string | undefined;
-  polywork: string | undefined;
+  title?: string;
+  twitter?: string;
+  youtube?: string;
+  twitch?: string;
+  github?: string;
+  website?: string;
+  polywork?: string;
 }
