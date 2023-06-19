7ded05207763946b89c1614b522546741033f798
Nick Taylor <nick@iamdeveloper.com>
2023-06-19 12:30:03 -0400

    chore: added Collab Lab mock interview

--- src/_data/talks.js
+++ src/_data/talks.js
@@ -1,5 +1,16 @@
 const rawTalks = [
   {
+    title: 'Career Lab Q2 2023: Watch Your Mentors Interview Each Other',
+    date: new Date('2023-06-05T12:00:00.000Z'),
+    video: {
+      type: 'youtube',
+      url: 'https://www.youtube.com/watch?v=M_UntxJu-s4',
+    },
+    venue: {name: 'The Collab Lab: Career Lab', url: 'https://the-collab-lab.codes/'},
+    summary: `In this session, Nick Taylor and EJ Mason run a mock interview where EJ is the interviewer, and Nick is the interviewee. They go over Nick's frontend take-home assignment, and EJ also asks Nick questions about frontend about accessibility, JavaScript, HTML, CSS, and React.`,
+    tags: ['career', 'interviewing'],
+  },
+  {
     title: 'Fresh: A New Full Stack Web Framework for Deno',
     date: new Date('2023-04-20T12:00:00.000Z'),
     video: {
