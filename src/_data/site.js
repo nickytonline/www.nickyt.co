c38ba3ea6527be8962ab0241e43221bbc92fd02e
Nick Taylor <nick@iamdeveloper.com>
2023-08-10 22:20:53 -0400

    fix: gets correct context environment variable now

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,16 @@
+const getSiteUrl = (context = process.env.CONTEXT) => {
+  console.log("context", context);
+
+  return context === "production"
+    ? process.env.URL
+    : process.env.DEPLOY_PRIME_URL ?? "http://localhost:8080";
+};
+
 module.exports = {
   showThemeCredit: true,
   name: "Just Some Dev",
   shortDesc: "Welcome to Nick Taylor's personal website.",
-  url:
-    process.env.context === "production"
-      ? process.env.URL
-      : process.env.DEPLOY_PRIME_URL ?? "http://localhost:8080",
+  url: getSiteUrl(),
   authorEmail: "nick@iamdeveloper.com",
   twitterHandle: "@nickytonline",
   mastodonHandle: "@nickytonline@toot.cafe",
