0df30318436fbe26c6e178f4c9bade81648e0391
Nick Taylor <nick@nickyt.co>
2023-08-23 18:08:19 -0400

    chore: removed URL check at beginning of edge functions

--- netlify/edge-functions/homepage/homepage.ts
+++ netlify/edge-functions/homepage/homepage.ts
@@ -12,12 +12,6 @@ if (!AIRTABLE_API_KEY || !AIRTABLE_STREAM_GUEST_BASE_ID) {
 }
 
 export default async (request: Request, context: Context) => {
-  const url = new URL(request.url);
-
-  if (!url.host.startsWith('www.nickyt.co')) {
-    return
-  }
-
   // Locale is the first language in the Accept-Language header or 'en-US' if not present
   const locale =
     request.headers.get("Accept-Language")?.split(",")[0] ||
