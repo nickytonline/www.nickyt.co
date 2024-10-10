c243054746ab618d4c3b00051a64ee9be22c0192
Nick Taylor <nick@nickyt.co>
2024-10-09 22:20:44 -0400

    Removes .genaiscript folder

--- src/_data/talks.js
+++ src/_data/talks.js
@@ -1,21 +1,5 @@
 const rawTalks = [
   {
-    title: "Why HTML Forms Still Rule the Web",
-    date: new Date("2024-10-03T09:00:00.000Z"),
-    slideDeck: "/slides/compose-24",
-    venue: {
-      name: "Netlify Compose 2024",
-      url: "https://www.netlify.com/compose",
-    },
-    video: {
-      type: "youtube",
-      url: "https://youtu.be/1U7b7SwKv_s",
-    },
-    summary:
-      "We've strayed too far from the simplicity and power of HTML forms. It’s time to undo the damage. Once relegated to basic data collection, forms offer a surprising depth of functionality that's being rediscovered by developers and embraced by modern frameworks like Astro, Fresh, Remix, and Next.js. We'll go back to the basics of HTML forms to dig into their history, look at the different input types, validation attributes, and advanced capabilities like the Constraint Validation API. Developers sometimes underestimate the power of CSS for building visually appealing and accessible forms. By using form-specific pseudo-classes like :focus, :hover, :invalid, and :valid, we can enhance the user experience without overcomplicating things by resorting to custom JavaScript. This not only simplifies code but also ensures a consistent user experience across browsers and devices, reinforcing the core strengths of HTML forms. We'll circle back to the present and see how frameworks like Astro, Fresh, Remix, and Next.js are leveraging forms via APIs like the FormData interface. Let’s rediscover the power of HTML forms and how they should be leveraged in modern web application development.",
-    tags: ["html", "forms", "css", "javascript", "frameworks"],
-  },
-  {
     title: "Getting the Most out of Open Source",
     date: new Date("2023-10-19T12:00:00.000Z"),
     slideDeck: "/hack-for-la-2023",
